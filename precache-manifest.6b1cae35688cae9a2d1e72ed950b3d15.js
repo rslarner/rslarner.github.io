66a56aeadca195916bcf437740dad8da28c2edd1
Ross Larner <rslarner@gmail.com>
2018-11-22 00:32:41 -0800

    Updates

---
+++ precache-manifest.6b1cae35688cae9a2d1e72ed950b3d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c08b791227bdc3d1684f",
+    "url": "/static/js/main.c08b7912.chunk.js"
+  },
+  {
+    "revision": "3dce74a569989e5eeda4",
+    "url": "/static/js/1.3dce74a5.chunk.js"
+  },
+  {
+    "revision": "c08b791227bdc3d1684f",
+    "url": "/static/css/main.d99a3957.chunk.css"
+  },
+  {
+    "revision": "09f60ea955fec4aad296da98d4734407",
+    "url": "/index.html"
+  }
+];
